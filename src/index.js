e03687fbd645aa2bffcf454097ef00fa50ab0b64
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2019-02-05 10:57:12 +0300

    Add PORT as env variable

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ app.use('*', (req, res) => {
 })
 
 const server = http.createServer(app);
+const port = process.env.PORT;
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 })
